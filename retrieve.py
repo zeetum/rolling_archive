e508388eccb5bdcaf65c64acc282b566c0f1ce9f
zeetum <timothy.blackburn@education.wa.edu.au>
2017-06-02 12:14:00 +0800

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -1,25 +1,40 @@
 import sys
 import datetime
 from calendar import monthrange
-
-# returns the time from today to retrieve
-def days_ago(retrieve_date):
-    today = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d")
-    retrieve = datetime.datetime.strptime(str(retrieve_date), "%Y-%m-%d")
-
-    return (today - retrieve).days
-
-
 # Calculates the day and month to retrieve from
-# return: %d-%m 
-def get_day_in_month():
-    months = [monthrange(datetime.date.today().year, datetime.date.today().month)[1],
-              monthrange(datetime.date.today().year, datetime.date.today().month -1)[1],
-              monthrange(datetime.date.today().year, datetime.date.today().month -2)[1],
-              monthrange(datetime.date.today().year, datetime.date.today().month -3)[1]]
-    print(months)
+# return: %d-%m
+def get_day_of_month(retrieve_date):
+ 
+        today = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d")
+        retrieve = datetime.datetime.strptime(str(retrieve_date), "%Y-%m-%d")
+        days_ago = (today - retrieve).days
+        
+        months = [monthrange(datetime.date.today().year, datetime.date.today().month)[1],
+        monthrange(datetime.date.today().year, datetime.date.today().month -1)[1],
+        monthrange(datetime.date.today().year, datetime.date.today().month -2)[1],
+        monthrange(datetime.date.today().year, datetime.date.today().month -3)[1]]
+        
+        month = 0
+        today = today.day
+        
+        if today - days_ago <= 0:
+                month += 1
+                days_ago -= today
+        else:
+                return [today - days_ago, 0]
+
+        while days_ago > 0 and month < 3:
+                if days_ago - months[month] < 0:
+                        break
+
+                days_ago -= months[month]
+                month += 1
+
+        if days_ago > months[3]:
+                return [months[3], 3]
+        else:
+                return [months[month] - days_ago, month]
 
 
 
-print(days_ago(sys.argv[1]))
-print(get_day_in_month())
+print(get_day_of_month(sys.argv[1]))
