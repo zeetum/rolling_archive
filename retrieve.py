800e14997b735c01655e82182c14e24f710dce63
zeetum <timothy.blackburn@education.wa.edu.au>
2017-06-02 13:48:04 +0800

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -4,19 +4,20 @@ from calendar import monthrange
 # Calculates the day and month to retrieve from
 # return: %d-%m
 def get_day_of_month(retrieve_date):
- 
+
         today = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d")
         retrieve = datetime.datetime.strptime(str(retrieve_date), "%Y-%m-%d")
         days_ago = (today - retrieve).days
-        
+        if days_ago < 0:
+                days_ago = 0
+
         months = [monthrange(datetime.date.today().year, datetime.date.today().month)[1],
         monthrange(datetime.date.today().year, datetime.date.today().month -1)[1],
         monthrange(datetime.date.today().year, datetime.date.today().month -2)[1],
         monthrange(datetime.date.today().year, datetime.date.today().month -3)[1]]
-        
+
         month = 0
         today = today.day
-        
         if today - days_ago <= 0:
                 month += 1
                 days_ago -= today
