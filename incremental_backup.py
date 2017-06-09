d14008c6ac30b919c62d4a00bc898a15113fbe51
zeetum <timothy.blackburn@education.wa.edu.au>
2017-06-09 13:27:07 +0800

    Create incremental_backup.py

--- incremental_backup.py
+++ incremental_backup.py
@@ -3,95 +3,49 @@
 from functools import reduce
 import os
 import tarfile
-import datetime
-from calendar import monthrange
 import xdelta3
 
 class Archive:
 
 	backup_location = ""
-	month_folders = []
+	creation_date = ""
 
-	def __init__(self, backup_location, months):
+	def __init__(self, backup_location):
 		self.backup_location = backup_location
-		self.month_folders = list(map(lambda month: backup_location + "/" + month, months))
-		__create_folders()
-
-	
-	# Creates the folder structure or does nothing if it already exists
-	def __create_folders(self):
-		if not os.path.exists(self.backup_location):
-                        os.makedirs(self.backup_location)
-
-		for month in self.month_folders:
-                	if not os.path.exists(month):
-                        	os.makedirs(month)
-
-				
-	# Returns a list of binary strings of data for the month from file
-	def __get_month(self, month):
-                month_data = []
 		
-                days_in_month = monthrange(datetime.date.today().year, datetime.date.today().month)[1]
-                for day in range(days_in_month):
-                        day_file = self.month_folders[month] + "/" + day
+		if not os.path.exists(backup_location):
+			os.makedirs(backup_location)
+			with open(backup_location + "/creation_date") as time_file:
+				self.creation_date = datetime.datetime.fromtimestamp(time.time()).strftime("%d-%m-%Y")
+				time_file.write(self.creation_date)
+		else:
+			with open(backup_location + "/creation_date") as time_file:
+				self.creation_date = time_file.read()
+                	
+	def __get_data(self, days_ago):
+                backup_data = []
+		
+                for day in range(days_ago):
+                        day_file = self.backup_location + "/" + day
                         if os.path.isfile(day_file):
                                 with open(day_file, 'rb') as f:
-                                        self.month_data.append(f.read())
+                                        self.backup_data.append(f.read())
                         else:
-                                self.month_data.append(b'')
+                                self.backup_data.append(b'')
                 
                 return month_data
-
-				
-	# Clears the month and flushes the file
-	def __clear_month(self, month):
-                days_in_month = monthrange(datetime.date.today().year, datetime.date.today().month)[1]
-                for day in range(days_in_month):
-                        day_file = self.month_folders[month] + "/" + str(day)
-			if os.path.isfile(day_file):
-                        	os.remove(day_file)
-
-
-	# Returns the (day, month) to retrieve from backup
-	def __get_day_location(self, backup_date):
-                today = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d")
-                retrieve = datetime.datetime.strptime(str(backup_date), "%d-%m-%Y")
-                days_ago = (today - retrieve).days
-                if days_ago < 0:
-                        days_ago = 0
-
-                months = [monthrange(datetime.date.today().year, datetime.date.today().month)[1],
-                monthrange(datetime.date.today().year, datetime.date.today().month - 1)[1],
-                monthrange(datetime.date.today().year, datetime.date.today().month - 2)[1],
-                monthrange(datetime.date.today().year, datetime.date.today().month - 3)[1]]
-
-                month = 0
-                today = today.day
-                if today - days_ago <= 0:
-                        month += 1
-                        days_ago -= today
-                else:
-                        return (today - days_ago, 0)
-                
-                while days_ago > 0 and month < 3:
-                        if days_ago - months[month] < 0:
-                                break
-
-                        days_ago -= months[month]
-                        month += 1
-                
-                if days_ago > months[3]:
-                	return (months[3], 3)
-                else:
-                	return (months[month] - days_ago, month)
-                	
-                	
+	
+	
         # Writes the file associated with backup_date to restore_file_location
-	def retrieve_day(self, restore_location, backup_date):
-		(day, month) = __get_day_location(self, backup_date)
-		month_data = __get_month(month)
-		day_data = reduce(xdelta3.decode, month_data[:day])
+	def retrieve_day(self, restore_location, retrieve_date):
+		today = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d")
+                retrieve_date = datetime.datetime.strptime(str(retrieve_date), "%d-%m-%Y")
+		days_ago = (today - retrieve).days
+		if (days_ago > (today - creation_date)):
+			days_ago = str(creation_date)
+		
+		backup_data = __get_data(days_ago)
+		day_data = reduce(xdelta3.decode, backup_data)
 		
 		# Write the days data to a temp file
 		temp_file = self.backup_location + "/temp.tar.xz"
