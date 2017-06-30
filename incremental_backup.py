ce5143d5347de07b23f9597350948b2374677476
zeetum <timothy.blackburn@education.wa.edu.au>
2017-06-30 10:26:07 +0800

    Update incremental_backup.py

--- incremental_backup.py
+++ incremental_backup.py
@@ -19,14 +19,13 @@ class Archive:
 		if not os.path.exists(backup_location):
 			os.makedirs(backup_location)
 		
-		if os.path.exists(backup_location + "/creation_date"):
-			with open(backup_location + "/creation_date") as time_file:
-				self.creation_date = datetime.datetime.strptime(time_file.read(), "%d-%m-%Y")
-		else:
-			with open(backup_location + "/creation_date", "w") as time_file:
-				self.creation_date = datetime.datetime.now().strftime("%d-%m-%Y")
-				time_file.write(self.creation_date)
-
+                if os.path.exists(backup_location + "/creation_date"):
+                        with open(backup_location + "/creation_date") as time_file:
+                                self.creation_date = datetime.datetime.strptime(time_file.read(), "%Y-%m-%d")
+                else:
+                        with open(backup_location + "/creation_date", "w") as time_file:
+                                self.creation_date = datetime.datetime.now()
+                                time_file.write(self.creation_date.strftime("%Y-%m-%d"))
 
 
 	# Returns an array of data from creation_date to retrieve_date
