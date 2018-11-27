de6a69573c5d2aa85d6d57d998a167a930ab9051
zeetum <timothy.blackburn@education.wa.edu.au>
2018-11-27 14:01:34 +0800

    Update archive.py

--- archive.py
+++ archive.py
@@ -21,13 +21,14 @@ class Archive:
 		if os.path.exists(backup_location + "/temp"):
 			os.remove(backup_location + "/temp")
 		
-                if os.path.exists(backup_location + "/creation_date"):
-                        with open(backup_location + "/creation_date") as time_file:
-                                self.creation_date = datetime.datetime.strptime(time_file.read(), "%Y-%m-%d")
-                else:
-                        with open(backup_location + "/creation_date", "w") as time_file:
-                                self.creation_date = datetime.datetime.now()
-                                time_file.write(self.creation_date.strftime("%Y-%m-%d"))
+		if os.path.exists(backup_location + "/creation_date"):
+			with open(backup_location + "/creation_date") as time_file:
+				self.creation_date = datetime.datetime.strptime(time_file.read(), "%Y-%m-%d")
+
+		else:
+			with open(backup_location + "/creation_date", "w") as time_file:
+				self.creation_date = datetime.datetime.now()
+				time_file.write(self.creation_date.strftime("%Y-%m-%d"))
 
 
 	# Returns a binary string of the day's data
